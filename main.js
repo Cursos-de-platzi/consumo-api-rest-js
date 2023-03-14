aa976c79cbfc40a5ab76aa87f4673674367f77b8
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-03-14 18:03:37 -0500

    vista la clase9, se estudió el metodo get

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log('Hello world')
-
 /*
 endpoints sin query parameters
 const API_URL = 'https://api.thecatapi.com/v1/images/search';
@@ -9,9 +7,9 @@ const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 */
 
 // con API_KEY
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_krhUpxDq3fu07pNXftZRxW8uYzUbaBpeScrPDskWc3YEPeBDnp14NQeM04yjXroV';
-
-
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_krhUpxDq3fu07pNXftZRxW8uYzUbaBpeScrPDskWc3YEPeBDnp14NQeM04yjXroV';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_krhUpxDq3fu07pNXftZRxW8uYzUbaBpeScrPDskWc3YEPeBDnp14NQeM04yjXroV';
+const spanError = document.getElementById('error');
 /*
 fetch nos devuelve una promesa, y las promesas podemos resolverlas con el metodo then.
 cuando cargamos un API, lo primero que tenemos que hacer es convertir esa respuesta a algo que js pueda entender como un objeto
@@ -25,7 +23,7 @@ const cambiarGato = () => fetch(API_URL)
         img.src = data[0].url;                       // le modificamos la propiedad src
     })
 */
-
+/*
 async function reload(){
     const res = await fetch(API_URL);      // estamos llamando a la API
     const data = await res.json();         // estamos convirtiendo eso a sintaxis que js pueda entender
@@ -40,9 +38,39 @@ async function reload(){
     img2.src = data[1].url;
     img3.src = data[2].url;
 }
-
+*/
 /*
 para que recien entremos a nuestra aplicación, no aparezca vacío, lo que podemos hacer es:
 llamar a la función apenas terminemos de cargar nuestro archivo js
 */
-reload();
+
+async function loadRandomMichis(){
+    const res = await fetch(API_URL_RANDOM);      // estamos llamando a la API
+    const data = await res.json();         // estamos convirtiendo eso a sintaxis que js pueda entender
+    console.log('random');
+    console.log(data);
+    //const img = document.querySelector('img');  cuando son varias imagenes, no me sirve el queryselector
+
+    // el res nos puede decir cómo quedó el status code, el backend nos puede dar un spoiler de como le fue a nuestra solicitud, antes de darnos los datos
+    if(res.status !== 200){
+       spanError.innerHTML = "Hubo un error: " + res.status; 
+    }
+    else {
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    }
+}
+
+async function loadFavoritesMichis(){
+    const res = await fetch(API_URL_FAVORITES);      // estamos llamando a la API
+    const data = await res.json();         // estamos convirtiendo eso a sintaxis que js pueda entender
+    console.log('favorites');
+    console.log(data);
+    if(res.status !== 200){
+        spanError.innerHTML = "Hubo un error: " + res.status + data.message; 
+     }
+}
+loadRandomMichis();
+loadFavoritesMichis();
